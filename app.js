de05ded1fccd74371de02a759588603a70e42700
jaeseong-CS <shimjs8@naver.com>
2019-08-26 16:22:42 +0900

    feat: swagger

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ import { join } from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { connect } from 'mongoose';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import router from './routes';
 import reset from './reset';
+
+const swaggerDefinition = {
+  info: {
+    description: 'Dimicheck Api Document System',
+    version: '1.0.0',
+    title: 'Dimicheck API',
+  },
+  host: 'localhost:3000',
+  basePath: '/',
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./config/document.yaml'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 const app = express();
 
@@ -16,7 +35,8 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(Static(join(__dirname, 'public')));
 
-app.use('/', router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api', router);
 
 connect(
   'mongodb://localhost:27017/test',
