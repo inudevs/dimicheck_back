16dc085af212cc7ba73bfd29837f2ecd96e06bff
jaeseong-CS <shimjs8@naver.com>
2019-08-27 21:50:34 +0900

    feat: Status divided into '1 time' and '2 time'

--- reset.js
+++ reset.js
@@ -3,24 +3,34 @@ import Member from './models/Member';
 import Status from './config/status';
 
 export default new CronJob({
-  cronTime: '0 50 19 * * 0-5',
+  // 일요일 ~ 금요일 7시 정각에 실행
+  cronTime: '0 0 7 * * 0-5',
+  // 실행할 내용
   onTick: async () => {
+    // 데이터베이스에서 Member값을 가져옴
     const member = await Member.find();
-    member.map((mem) => ({
-      grade: mem.grade,
-      class: mem.class,
-      total: 0, // 디미고인에서 가져오기
-      current: 0, // 디미고인에서 가져오기
-      status: () => {
-        const s = [];
-        for (let i = 0; i < 35; i += 1) {
-          s[i] = Status.class;
-        }
-        return s;
-      },
-    }));
+    // 반의 개수만큼 (18개)
+    for (let i = 0; i < member.length; i += 1) {
+      // 모든 학생의 상태를 Class로 초기화
+      const s = [];
+      for (let j = 0; j < 35; j += 1) {
+        s[j] = [Status.class, Status.class];
+      }
+      Member.updateOne(
+        member[i],
+        {
+          status: s,
+        },
+        (err) => {
+          if (err) {
+            // eslint-disable-next-line no-console
+            console.error(err);
+          }
+        },
+      );
+    }
     // eslint-disable-next-line no-console
-    console.log('reset db');
+    console.log('reset database');
   },
   start: false,
   timeZone: 'Asia/Seoul',
